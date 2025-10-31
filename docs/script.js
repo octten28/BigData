27d5a693132caf1405cf22857d5e866e1bc800bc
AdrianT <33002025+octten28@users.noreply.github.com>
2025-10-31 08:49:38 +0000

    Embed XML directly for GitHub Pages compatibility

--- docs/script.js
+++ docs/script.js
@@ -1,18 +1,28 @@
-fetch('./data.xml')
-  .then(response => response.text())
-  .then(str => (new window.DOMParser()).parseFromString(str, "text/xml"))
-  .then(data => {
-    const records = data.getElementsByTagName("record");
-    let tableHTML = "<table><tr><th>Name</th><th>Role</th><th>Location</th></tr>";
+const xmlString = `
+<records>
+  <record>
+    <name>Adrian Tolentino</name>
+    <role>Technical Project Manager</role>
+    <location>Metro Manila</location>
+  </record>
+  <record>
+    <name>Sattvik</name>
+    <role>Destroyer</role>
+    <location>Mars</location>
+  </record>
+</records>
+`;
 
-    for (let i = 0; i < records.length; i++) {
-      const name = records[i].getElementsByTagName("name")[0].textContent;
-      const role = records[i].getElementsByTagName("role")[0].textContent;
-      const location = records[i].getElementsByTagName("location")[0].textContent;
-      tableHTML += `<tr><td>${name}</td><td>${role}</td><td>${location}</td></tr>`;
-    }
+const parser = new DOMParser();
+const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+const records = xmlDoc.getElementsByTagName("record");
 
-    tableHTML += "</table>";
-    document.getElementById("xml-table").innerHTML = tableHTML;
-  })
-  .catch(err => console.error("XML fetch error:", err));
+let tableHTML = "<table><tr><th>Name</th><th>Role</th><th>Location</th></tr>";
+for (let i = 0; i < records.length; i++) {
+  const name = records[i].getElementsByTagName("name")[0].textContent;
+  const role = records[i].getElementsByTagName("role")[0].textContent;
+  const location = records[i].getElementsByTagName("location")[0].textContent;
+  tableHTML += `<tr><td>${name}</td><td>${role}</td><td>${location}</td></tr>`;
+}
+tableHTML += "</table>";
+document.getElementById("xml-table").innerHTML = tableHTML;
